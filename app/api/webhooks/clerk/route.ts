da37be8f4c7aca88e2ecbbd357608cc80916daad
vinodpatil2002 <vinodnspatil@gmail.com>
2024-08-28 20:54:28 +0530

    refactor: Update clerk webhook route to handle null values for first name and last name

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,5 +1,4 @@
 /* eslint-disable camelcase */
-// import { clerkClient } from "@clerk/nextjs";
 import { clerkClient, WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
@@ -72,8 +71,8 @@ export async function POST(req: Request) {
             clerkId: id,
             email: email_addresses[0].email_address,
             username: username!,
-            firstName: first_name,
-            lastName: last_name,
+            firstName: first_name ?? "", // Provide a default empty string if null
+            lastName: last_name ?? "", // Provide a default empty string if null
             photo: image_url,
         };
 
@@ -96,8 +95,8 @@ export async function POST(req: Request) {
         const { id, image_url, first_name, last_name, username } = evt.data;
 
         const user = {
-            firstName: first_name,
-            lastName: last_name,
+            firstName: first_name ?? "", // Provide a default empty string if null
+            lastName: last_name ?? "", // Provide a default empty string if null
             username: username!,
             photo: image_url,
         };
